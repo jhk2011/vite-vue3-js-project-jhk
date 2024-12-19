f248fdb2cf1c15041af7c9b80ea5ebcec095ed93
jhk2011 <27795718+jhk2011@users.noreply.github.com>
2024-12-19 01:32:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        demo: resolve(__dirname, 'demo.html'),
+      }
+    }
+  }
+})
